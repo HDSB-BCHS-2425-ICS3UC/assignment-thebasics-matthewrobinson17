35b13a27b161f293bf31c73898ba989a1a817e09
matthewrobinson17 <1robinsonmat@hdsb.ca>
2025-02-28 13:57:56 -0500

    Assignment intro to basics
    Hopefully fixed now

--- Math.py
+++ Math.py
@@ -12,3 +12,11 @@ R = 3-2
 T = 3/4
 Y = 3%4
 U = math.sqrt(9)
+
+print("3+4=",Q)
+print("3*4=",W)
+print('3**4=',E)
+print("3-2=",R)
+print("3/4=",T)
+print("3%4=",Y)
+print("math.sqrt(9)=",Y)
--- volume of cube andf others.py
+++ volume of cube andf others.py
@@ -1,21 +1,25 @@
 #matthew Robinson
 #2025-02-25
 
+#Imports Pi for my codes
 import math
 Pi=math.pi
 
+#Allows for the storage of the mesurments as floats
 A = float(input("Area: "))
 R = float(input("Radius: "))
 H = float(input("Height: "))
 
-
+#Equations for the differnt shapes and alocates a number for each shape
 one=(A**3)
 two=(4/3*Pi*R**2)
 three=(1/3*Pi*R**2*H)
 four=(Pi*R**2*H)
 
+# give instructions and makes I an variable
 I=input('''Type "Cube" or "Sphere" or "Cone" or "Cylinder" ''')
 
+#depending on what I is equal to will print out the anwser to the equation
 if I == "Cube":
  print(one)
 if I == "Sphere":
